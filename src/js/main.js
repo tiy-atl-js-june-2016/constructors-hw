9504300fede6cb00a3d0bc2c6e7f85c5f9998131
Brit Butler <redline6561@gmail.com>
2016-07-04 22:24:26 -0400

    Add normal mode.

---
+++ src/js/main.js
@@ -0,0 +1,64 @@
+import {it, expect} from "./test_lib";
+// import {coffee, human} from "./bonus";
+
+// Constructors
+// Only add code to *THIS* section!
+
+///
+///
+///
+///
+
+// Do not ADD or MODIFY code below this line :)
+// Dogs
+let sadie = new Dog({
+  color: 'black',
+  hungry: false
+});
+
+let moonshine = new Dog({
+  color: 'blue-red'
+});
+
+let atticus = new Dog();
+
+// Humans
+let mason = new Human();
+
+let julia = new Human({
+  cool: true
+});
+
+// Tests
+it("should make Sadie happy when Mason pets her", function() {
+  expect(sadie.status).toBe('normal');
+  mason.pet(sadie);
+  expect(sadie.status).toBe('happy');
+});
+
+it("should make Sadie black", function(){
+  expect(sadie.color).toBe('black');
+});
+
+it("should be make Moonshine hungry and Sadie not hungry", function() {
+  expect(moonshine.hungry).toBe(true);
+  expect(sadie.hungry).toBe(false);
+});
+
+it("should make Moonshine no longer hungry when you feed him", function() {
+  julia.feed(moonshine);
+  expect(moonshine.hungry).toBe(false);
+});
+
+
+it("should not affect Atticus and Moonshine's owner properties when setting Mason as Sadie's owner ", function() {
+  sadie.owner = mason;
+  expect(moonshine.owner).toBe(undefined);
+  expect(atticus.owner).toBe(undefined);
+});
+
+it("should make Julia cool and Mason not cool", function() {
+  sadie.owner = mason;
+  expect(julia.cool).toBe(true);
+  expect(mason.cool).toBe(false);
+});
