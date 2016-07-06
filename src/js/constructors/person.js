7556602821fb99cd7f2f2aa045370388e83c9c2d
Brit Butler <redline6561@gmail.com>
2016-07-06 10:11:25 -0400

    Normal mode done

--- src/js/constructors/person.js
+++ src/js/constructors/person.js
@@ -1,3 +1,13 @@
-function Person () {
-  
+function Person (options) {
+  options = options || {};
+  this.cool = options.cool || false;
+  this.pet = function (dog) {
+    dog.cuteness = "100 emoji";
+    dog.status = "happy";
+  };
+  this.feed = function (animal) {
+    animal.hungry = false;
+  };
 };
+
+export {Person};
